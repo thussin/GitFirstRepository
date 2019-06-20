fcf787e7bcebef7d42b90d61d86d957ac280070b
Hussain <thussain@teksystems.com>
2019-06-20 13:19:05 -0400

    Seconf git program

---
+++ src/gitsessionpKg/another.java
@@ -0,0 +1,11 @@
+package gitsessionpKg;
+
+public class another {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("New java program - working");
+
+	}
+
+}
