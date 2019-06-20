38045c7ac1680685ac999056111d38b368e02f91
Hussain <thussain@teksystems.com>
2019-06-20 12:57:10 -0400

    Sending my first git program

---
+++ src/gitsessionpKg/firstgitprogram.java
@@ -0,0 +1,11 @@
+package gitsessionpKg;
+
+public class firstgitprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Learning");
+
+	}
+
+}
